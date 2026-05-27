6e6a0dc75421b0859f4f1a55d0773045c4af9c8a
Juiocezar <juliocezar9770@gmail.com>
2026-05-27 16:13:24 -0300

    Implementei o do-while no lugar do while, e filtrei a resposta do usuario

--- trabalho.c
+++ trabalho.c
@@ -4,17 +4,22 @@ int main() {
 
 int opcao_menu;
 
-while(opcao_menu != 6){
-    printf("=========Menu=========\n");
+do{
+    printf("\n=========Menu=========\n");
     printf("1- Cadastrar registro\n");
     printf("2- Listar registros\n");
     printf("3- Buscar registro\n");
     printf("4- ar registro\n");
     printf("5- Remover registro\n");
     printf("6- Sair\n");
-    printf("Escolha uma dessa opcoes :");
+    printf("Escolha uma dessa opcoes :\n");
     scanf("%i", &opcao_menu);
-}
+    
+    if(opcao_menu < 1 || opcao_menu > 6) {
+        printf("\n\nDigite uma das opcoes, entre 1 e 6 Por favor!\n");
+
+} 
+}while(opcao_menu != 6);
     
 
     return 0;
